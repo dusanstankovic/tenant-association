5cace7a0b2db9d327998a9ad19bffdd0cca7fac2
Dusan Stankovic <sdusan@gmail.com>
2020-04-09 12:13:14 +0200

    Initial project add

---
+++ src/main/java/dev/themighty/tenantassociation/TenantAssociationApplication.java
@@ -0,0 +1,13 @@
+package dev.themighty.tenantassociation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenantAssociationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TenantAssociationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/themighty/tenantassociation/TenantAssociationApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.themighty.tenantassociation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenantAssociationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
