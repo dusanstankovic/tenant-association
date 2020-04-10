7f8b8fef3d11bcb627ecb3d755e7287a23623c05
Dusan Stankovic <sdusan@gmail.com>
2020-04-10 18:29:36 +0200

    Implemented services (#8)

---
+++ src/main/java/dev/themighty/tenantassociation/services/TenantService.java
@@ -0,0 +1,8 @@
+package dev.themighty.tenantassociation.services;
+
+import dev.themighty.tenantassociation.model.Tenant;
+
+public interface TenantService extends CrudService<Tenant, Long> {
+
+    // TODO: Implement TenantService
+}
---
+++ src/main/java/dev/themighty/tenantassociation/services/UnitService.java
@@ -0,0 +1,8 @@
+package dev.themighty.tenantassociation.services;
+
+import dev.themighty.tenantassociation.model.Unit;
+
+public interface UnitService extends CrudService<Unit, Long> {
+
+    // TODO: Implement UnitService
+}
