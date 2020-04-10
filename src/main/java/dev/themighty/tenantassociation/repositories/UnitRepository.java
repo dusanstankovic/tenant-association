77706e5e5fbbe334ee7e25b58e36ce1c3208acf9
Dusan Stankovic <sdusan@gmail.com>
2020-04-10 16:41:22 +0200

    Implemented repositories. Closes #4.

---
+++ src/main/java/dev/themighty/tenantassociation/repositories/UnitRepository.java
@@ -0,0 +1,8 @@
+package dev.themighty.tenantassociation.repositories;
+
+import dev.themighty.tenantassociation.model.Unit;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitRepository extends CrudRepository<Unit, Long> {
+
+}
