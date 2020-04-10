c31d1f917870d41fb33148ba0aa21e57ec5f701c
Dusan Stankovic <sdusan@gmail.com>
2020-04-10 18:29:00 +0200

    Implemented services. Closes #7.

---
+++ src/main/java/dev/themighty/tenantassociation/services/MeetingService.java
@@ -0,0 +1,8 @@
+package dev.themighty.tenantassociation.services;
+
+import dev.themighty.tenantassociation.model.Meeting;
+
+public interface MeetingService extends CrudService<Meeting, Long> {
+
+    // TODO: Implement MeetingService
+}
