97f24e5decc79744421f678cda5f1216683ad4ce
Dusan Stankovic <sdusan@gmail.com>
2020-04-14 09:20:54 +0200

    Refactored Map Services and map service tests.

--- src/main/java/dev/themighty/tenantassociation/services/map/MeetingMapService.java
+++ src/main/java/dev/themighty/tenantassociation/services/map/MeetingMapService.java
@@ -2,8 +2,6 @@ package dev.themighty.tenantassociation.services.map;
 
 import dev.themighty.tenantassociation.model.Meeting;
 import dev.themighty.tenantassociation.services.MeetingService;
-import dev.themighty.tenantassociation.services.TenantService;
-import dev.themighty.tenantassociation.services.UnitService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
@@ -12,14 +10,6 @@ import java.util.Set;
 @Service
 @Profile({"default", "map"})
 public class MeetingMapService extends AbstractMapService<Meeting, Long> implements MeetingService {
-
-    private final TenantService tenantService;
-    private final UnitService unitService;
-
-    public MeetingMapService(TenantService tenantService, UnitService unitService) {
-        this.tenantService = tenantService;
-        this.unitService = unitService;
-    }
 
     @Override
     public Set<Meeting> findAll() {
